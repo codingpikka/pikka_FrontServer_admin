4ba4df4a70107e255858442e9c3bdb82c6007647
jinny <eujin.mstory@gmail.com>
2024-07-21 23:04:25 +0900

    first commit

---
+++ pikka-front/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Pikkachu from './components/pikkachu.vue'
+
+
+</script>
+
+<template>
+  <p>1</p>
+    <Pikkachu />
+
+  <p>2</p>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ pikka-front/src/components/pikkachu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="test">
+        <h4>피카코딩 깃test</h4>
+        <h5>은진test1</h5>
+    </div>
+</template>
+
+
+<style>
+.test {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+</style>
