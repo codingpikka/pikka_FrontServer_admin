1780b99645b6d560941873e2adc6e1759bd53d3d
wsy <wsy9088@gmail.com>
2024-07-23 11:57:53 +0900

    sy_pikkabody

---
+++ pikka-front/src/components/pikkabody.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pikka_body">
+      <div> body 영역 </div>
+    </div>
+        
+</template>
+
+
+<style>
+.pikka_body{
+  width: 100%;
+  display: flex;
+  padding: 20px;
+  
+}
+
+</style>
